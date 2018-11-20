f9364c90db36940e8bc2ce9512268650502e9905
Chendida <495518592@qq.com>
2018-11-20 17:53:47 +0800

    添加垂直纵向TabLayout框架，添加导航界面的布局

--- app/src/main/java/helloworld/zq/com/kotlindemo/ui/fragment/NavigationFragment.kt
+++ app/src/main/java/helloworld/zq/com/kotlindemo/ui/fragment/NavigationFragment.kt
@@ -2,11 +2,24 @@ package helloworld.zq.com.kotlindemo.ui.fragment
 
 import helloworld.zq.com.kotlindemo.R
 import helloworld.zq.com.kotlindemo.base.BaseFragment
+import helloworld.zq.com.kotlindemo.mvp.contract.NavigationContract
+import helloworld.zq.com.kotlindemo.mvp.model.bean.NavigationBean
 
 /**
  * 导航
  */
-class NavigationFragment : BaseFragment() {
+class NavigationFragment : BaseFragment(),NavigationContract.View {
+    override fun setNavigationData(list: List<NavigationBean>) {
+    }
+
+    override fun showLoading() {
+    }
+
+    override fun hideLoading() {
+    }
+
+    override fun showError(errMsg: String) {
+    }
 
     companion object {
         fun getInstance() : NavigationFragment = NavigationFragment()
